300c870419641f7acc4383438ecdf044001de494
AndrewSushyi <sushyi_av@ukr.net>
2020-01-02 21:30:40 +0200

    add test ValidLogin Full

--- src/test/java/loginTest/validLogin.java
+++ src/test/java/loginTest/validLogin.java
@@ -1,6 +1,8 @@
 package loginTest;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,8 +10,6 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class validLogin {
-
-
     @Test
     public void validLogin(){
        // loginPage.openPage("https://try.flowfinity.com/");
@@ -21,6 +21,15 @@ public class validLogin {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriver.get("https://try.flowfinity.com/");
 
+        webDriver.findElement(By.name("UserOnlyName")).clear();
+        webDriver.findElement(By.name("UserOnlyName")).sendKeys("qasushyi\\sushyi");
+
+        webDriver.findElement(By.name("UserOnlyPassword")).clear();
+        webDriver.findElement(By.name("UserOnlyPassword")).sendKeys("Nirvana123");
+
+        webDriver.findElement(By.id("submit-button")).click();
+
+        Assert.assertTrue("Нет Статуса авторизирЮзера", webDriver.findElement(By.xpath(".//*[@class='logon-status']")).isDisplayed());
 
         webDriver.quit();
     }
